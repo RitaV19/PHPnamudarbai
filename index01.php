f72b03198c3c3e8ac77a5febfa25eff0be184806
RitaV19 <53056672+RitaV19@users.noreply.github.com>
2019-08-13 23:07:48 +0300

    PHP01

---
+++ index01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		$salis = 'Lietuva';
+		$miestas = 'Vilnius';
+		$populiacija = 574221;
+	?>
+
+	<ul>
+		<li><?php echo $salis; ?></li>
+		<li><?php echo $miestas; ?></li>
+		<li><?php echo $populiacija; ?></li>
+	</ul>
+
+
+</body>
+</html>
