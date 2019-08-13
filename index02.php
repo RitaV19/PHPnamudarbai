fcf037cad7dadbe5f864210a6277ae4751e0feb3
RitaV19 <53056672+RitaV19@users.noreply.github.com>
2019-08-13 23:08:29 +0300

    PHP02

---
+++ index02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		$salis = 'Lietuva';
+		$miestas = 'Vilnius';
+		$populiacija = 574221;
+	?>
+
+	<p><?php echo $miestas. ' - Lietuvos sostinė'?>	</p>
+
+	<p><?php echo "$miestas ir $salis sparčiai auga"?></p>
+
+</body>
+</html>
