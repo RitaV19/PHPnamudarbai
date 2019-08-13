186ee1def46e7a4666e4e905783257bc44414d1f
RitaV19 <53056672+RitaV19@users.noreply.github.com>
2019-08-13 23:08:57 +0300

    PHP03

---
+++ index03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		$x = 10;
+		$y = 7;
+	?>
+
+	<p><?php $z = $x + $y; echo "$x + $y = $z";?></p>
+	<p><?php $z = $x - $y; echo "$x - $y = $z";?></p>
+	<p><?php $z = $x * $y; echo "$x * $y = $z";?></p>
+	<p><?php $z = $x / $y; echo "$x / $y = $z";?></p>
+	<p><?php $z = $x % $y; echo "$x % $y = $z";?></p>
+
+</body>
+</html>
