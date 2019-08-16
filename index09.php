fc5b4fac5eb58c6805742284d3885e8ef179b948
RitaV19 <53056672+RitaV19@users.noreply.github.com>
2019-08-16 22:59:23 +0300

    Add files via upload

---
+++ index09.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		$ceu = array("Italija"=>"Roma", "Liuksemburgas"=>"Liuksemburgas", "Belgija"=> "Briuselis", "Danija"=>"Kopenhaga", "Suomija"=>"Helsinkis", "Prancūzija" => "Paryžius", "Slovakija"=>"Bratislava", "Slovenija"=>"Liublijana", "Vokietija" => "Berlynas", "Graikija" => "Atėnai", "Airija"=>"Dublinas", "Nyderlandai"=>"Amsterdamas", "Portugalija"=>"Lisabona", "Ispanija"=>"Madridas", "Švedija"=>"Stokholmas", "Didžioji Britanija"=>"Londonas", "Kipras"=>"Nikosija", "Lietuva"=>"Vilnius", "Čekija"=>"Praha", "Estija"=>"Talinas", "Vengrija"=>"Budapeštas", "Latvija"=>"Ryga", "Malta"=>"Valeta", "Austrija" => "Viena", "Bulgarija" =>"Sofija", "Rumunija" => "Bukareštas", "Lenkija"=>"Varšuva") ;
+
+
+
+		asort($ceu) ;
+			foreach($ceu as $countryname => $capital){
+			echo "$countryname - $capital"."<br>" ;
+			}
+	?>
+
+	</body>
+</html>
