b0a98328977e21218e4bfcbf9bf5f1d985c01d0e
RitaV19 <53056672+RitaV19@users.noreply.github.com>
2019-08-21 22:40:06 +0300

    Add files via upload

---
+++ index12.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	
+	<form action="index12date.php" method="post">
+			Vardas: <input type="text" name="vardas"/><br><br>
+			Pavarde: <input type="text" name="pavarde"/><br><br>
+			<input type="submit" value="submit">
+	</form>		
+<!--	<?php echo $_POST["vardas"]; ?>-->
+<!--	<?php echo $_POST["pavarde"]; ?>-->
+	
+
+</body>
+</html>
---
+++ index12date.php
@@ -0,0 +1,21 @@
+	<?php
+		$vardas = $_POST['vardas'];
+		$pavarde = $_POST['pavarde'];
+	?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		echo $vardas. " " .$pavarde;
+	?><br>
+	<a href="index12.php">Back</a>
+			
+</body>
+</html>
